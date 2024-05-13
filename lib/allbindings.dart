c691bcc48f993e90b4ff96bfbc4100491c982cb8
AhmedRizwan23 <ahmedrixwan@gmail.com>
2024-05-13 21:08:39 +0500

    ..

--- lib/allbindings.dart
+++ lib/allbindings.dart
@@ -4,6 +4,12 @@ import 'package:getx_tutorial/bindcontroller.dart';
 class Allbindings extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut(() => Bindcontroller());
+    Get.lazyPut(() => Bindcontroller(), fenix: true);
+
+// In GetX, the fenix parameter is used in conjunction with the Get.lazyPut() method. It determines whether to recreate an instance of a dependency if it's requested again after being previously disposed of.
+
+// When you use Get.lazyPut() to register a dependency, you're telling GetX to lazily instantiate the dependency. This means that the dependency is only created when it's first requested, and subsequent requests return the same instance.
+
+// The fenix parameter, when set to true, ensures that even if the dependency has been disposed
   }
 }
--- lib/bindings.dart
+++ lib/bindings.dart
@@ -2,12 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:getx_tutorial/bindcontroller.dart';
 
+// Bindcontroller controller = Get.find();
+
 class Bindingspage extends StatelessWidget {
-  Bindingspage({super.key});
-  Bindcontroller controller = Get.find();
+  const Bindingspage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // Call Get.find() inside the build method
+    print("get find");
+    Bindcontroller controller = Get.find();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("GET x binding"),
